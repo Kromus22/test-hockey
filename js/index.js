594fd86f82552232fbbd57478bb591f81d4147b5
Kromus22 <kromus2@gmail.com>
2022-12-17 23:13:06 +0500

    +2 blocks, fixed slider

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 const swiper = new Swiper('.swiper', {
-  loop: true,
-  slidesPerView: 1,
-  spaceBetween: 20,
-
+  effect: "coverflow",
+  centeredSlides: true,
+  slidesPerView: "auto",
+  spaceBetween: 250,
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
   navigation: {
     nextEl: '.swiper-button-right',
     prevEl: '.swiper-button-left',
